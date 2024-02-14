880b4297f5882ab771698197260c35743bc1899f
Moises Lopez <mdlopezme@gmail.com>
2024-02-14 15:07:22 -0800

    inherent packetsubscriber

--- src/Controller/controller.cpp
+++ src/Controller/controller.cpp
@@ -8,4 +8,69 @@ namespace tritonai::gkc
   {
     std::cout << "Controller created" << std::endl;
   }
+
+  void Controller::packet_callback(const Handshake1GkcPacket &packet)
+  {
+    std::cout << "Handshake1GkcPacket received" << std::endl;
+  }
+
+  void Controller::packet_callback(const Handshake2GkcPacket &packet)
+  {
+    std::cout << "Handshake2GkcPacket received" << std::endl;
+  }
+
+  void Controller::packet_callback(const GetFirmwareVersionGkcPacket &packet)
+  {
+    std::cout << "GetFirmwareVersionGkcPacket received" << std::endl;
+  }
+
+  void Controller::packet_callback(const FirmwareVersionGkcPacket &packet)
+  {
+    std::cout << "FirmwareVersionGkcPacket received" << std::endl;
+  }
+
+  void Controller::packet_callback(const ResetMcuGkcPacket &packet)
+  {
+    std::cout << "ResetMcuGkcPacket received" << std::endl;
+  }
+
+  void Controller::packet_callback(const HeartbeatGkcPacket &packet)
+  {
+    std::cout << "HeartbeatGkcPacket received" << std::endl;
+  }
+
+  void Controller::packet_callback(const ConfigGkcPacket &packet)
+  {
+    std::cout << "ConfigGkcPacket received" << std::endl;
+  }
+
+  void Controller::packet_callback(const StateTransitionGkcPacket &packet)
+  {
+    std::cout << "StateTransitionGkcPacket received" << std::endl;
+  }
+
+  void Controller::packet_callback(const ControlGkcPacket &packet)
+  {
+    std::cout << "ControlGkcPacket received" << std::endl;
+  }
+
+  void Controller::packet_callback(const SensorGkcPacket &packet)
+  {
+    std::cout << "SensorGkcPacket received" << std::endl;
+  }
+
+  void Controller::packet_callback(const Shutdown1GkcPacket &packet)
+  {
+    std::cout << "Shutdown1GkcPacket received" << std::endl;
+  }
+
+  void Controller::packet_callback(const Shutdown2GkcPacket &packet)
+  {
+    std::cout << "Shutdown2GkcPacket received" << std::endl;
+  }
+
+  void Controller::packet_callback(const LogPacket &packet)
+  {
+    std::cout << "LogPacket received" << std::endl;
+  }
 } // namespace tritonai::gkc
--- src/Controller/controller.hpp
+++ src/Controller/controller.hpp
@@ -2,12 +2,29 @@
 #define CONTROLLER_HPP
 
 #include "Comm/comm.hpp"
+#include "tai_gokart_packet/gkc_packet_subscriber.hpp"
 namespace tritonai::gkc
 {
-  class Controller
+  class Controller :
+    public GkcPacketSubscriber
   {
     public:
       Controller();
+
+    protected:
+      void packet_callback(const Handshake1GkcPacket & packet);
+      void packet_callback(const Handshake2GkcPacket & packet);
+      void packet_callback(const GetFirmwareVersionGkcPacket & packet);
+      void packet_callback(const FirmwareVersionGkcPacket & packet);
+      void packet_callback(const ResetMcuGkcPacket & packet);
+      void packet_callback(const HeartbeatGkcPacket & packet);
+      void packet_callback(const ConfigGkcPacket & packet);
+      void packet_callback(const StateTransitionGkcPacket & packet);
+      void packet_callback(const ControlGkcPacket & packet);
+      void packet_callback(const SensorGkcPacket & packet);
+      void packet_callback(const Shutdown1GkcPacket & packet);
+      void packet_callback(const Shutdown2GkcPacket & packet);
+      void packet_callback(const LogPacket & packet);
   };
 }
 
